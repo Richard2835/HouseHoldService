f80c557ec004a0c03dbb844cc91423e6c195edc0
Richard Labat <riso2835@azet.sk>
2021-06-29 23:39:28 +0200

    hotova praca

--- src/main/java/sk/fri/uniza/model/IotNode.java
+++ src/main/java/sk/fri/uniza/model/IotNode.java
@@ -1,11 +1,35 @@
 package sk.fri.uniza.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.validator.constraints.NotEmpty;
 import javax.persistence.*;
 
+@org.hibernate.annotations.NamedQueries({
+        @org.hibernate.annotations.NamedQuery(name = "IotNode_AllIoTNodes",
+                query = "from IotNode"),
+        @org.hibernate.annotations.NamedQuery(
+                name = "IoTNode_findByHouseHold",
+                query = "from IotNode where houseHold = :houseHold"),
+        @org.hibernate.annotations.NamedQuery(name = "IotNode_findById",
+                query = "from IotNode where id = :id"),
+})
+
+@Entity
 public class IotNode {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @ApiModelProperty(accessMode = ApiModelProperty.AccessMode.READ_ONLY)
+    // Swagger nebude zobrazovať atribút
 
     private Long id;
+    @NotEmpty
+    @ApiModelProperty(example = "IoTNode") // Príklad pre swagger doku.
     private String Name;
+    @ManyToOne (fetch = FetchType.EAGER)
+    @JoinColumn(name = "household_id", nullable = false)
+    //@JsonIgnore // Ignorovanie danej premenej z pohladu Serializacie do
+    // Objektu JSON. Gneroval by sa obrovský JSON a dochádzalo by aj k zacykleniu
     private HouseHold houseHold;
 
     public Long getId() {
@@ -30,5 +54,18 @@ public class IotNode {
 
     public void setHouseHold(HouseHold houseHold) {
         this.houseHold = houseHold;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        IotNode iotNode = (IotNode) o;
+        return id != null ? id.equals(iotNode.id) : iotNode.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
     }
 }
--- src/main/java/sk/fri/uniza/resources/IoTNodeResource.java
+++ src/main/java/sk/fri/uniza/resources/IoTNodeResource.java
@@ -6,10 +6,17 @@ import io.swagger.annotations.ApiOperation;
 import sk.fri.uniza.db.IotNodeDAO;
 import sk.fri.uniza.model.IotNode;
 
+import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
+
+@Api("/IotNode") // Swagger
+@Path("/IotNode") // Koreňová adresa kolekcie koncových bodov
+// pre prístup k zdrojom domácností // Súčasť JAX-RS
+@Produces(MediaType.APPLICATION_JSON)// Výstupné dáta sú vo forme JSON //JAX-RS
+@Consumes(MediaType.APPLICATION_JSON) //Vstupné dáta sú vo forme JSON //JAX-RS
 public class IoTNodeResource {
 
     private IotNodeDAO iotNodeDAO;
@@ -18,20 +25,33 @@ public class IoTNodeResource {
         this.iotNodeDAO = iotNodeDAO;
     }
 
+    @POST /*JAX-RS*/
+    @UnitOfWork //Otvorí novú hibernate session // Dropwizard
+    @ApiOperation(value = "Prida novy IoTNode")
     public IotNode createIotNode(IotNode iotNode) {
-        return null;
+        return iotNodeDAO.create(iotNode);
     }
 
-    public IotNode updateIotNode(IotNode iotNode) {
-        return null;
+    @PUT /*JAX-RS*/
+    @UnitOfWork //Otvorí novú hibernate session // Dropwizard
+    @ApiOperation(value = "Upravi existujuci IoTNode")
+    public IotNode updateIotNode(@Valid IotNode iotNode) {
+        return iotNodeDAO.update(iotNode);
     }
 
-    public IotNode findIotNode(Long id) {
-        return null;
+    @GET /*JAX-RS*/
+    @Path("{id}")
+    @UnitOfWork //Otvorí novú hibernate session // Dropwizard
+    @ApiOperation(value = "Zobrazi IoTNode podla ID")
+    public IotNode findIotNode(@PathParam("id") Long id) {
+        return iotNodeDAO.findById(id);
     }
 
+    @GET /*JAX-RS*/
+    @UnitOfWork //Otvorí novú hibernate session // Dropwizard
+    @ApiOperation(value = "Zobrazi vsetky IoTNody")
     public List<IotNode> allIotNodes() {
-        return null;
+        return iotNodeDAO.allIotNodes();
     }
 
 }
